83beae6cb399fc840f5c81b0092d342a48d35fa3
Mandino <dino@misfit-inc.com>
2014-10-21 16:22:21 +0800

    several changes

---
+++ sites/all/modules/calendar/theme/calendar-month-col.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Template to display a column
+ * 
+ * - $item: The item to render within a td element.
+ */
+$id = (isset($item['id'])) ? 'id="' . $item['id'] . '" ' : '';
+$date = (isset($item['date'])) ? ' data-date="' . $item['date'] . '" ' : '';
+$day = (isset($item['day_of_month'])) ? ' data-day-of-month="' . $item['day_of_month'] . '" ' : '';
+$headers = (isset($item['header_id'])) ? ' headers="'. $item['header_id'] .'" ' : '';
+?>
+<?php print $item['entry'] ?>
